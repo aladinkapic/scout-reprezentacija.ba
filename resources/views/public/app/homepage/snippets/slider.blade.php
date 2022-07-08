7bcc78028b7d5ad0e07e3bcc77a60ba1d524b9a1
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2022-07-08 23:55:35 +0200

    Slider done + mobile friendly - done

--- resources/views/public/app/homepage/snippets/slider.blade.php
+++ resources/views/public/app/homepage/snippets/slider.blade.php
@@ -11,31 +11,66 @@
 {{--  </div>--}}
 {{--</div>--}}
 
-<section class="hero container-fluid border-1">
-    <div class="container">
-        <div class="row d-grid g-4 align-items-end">
-            <div class="col-sm-12 headline">
-                <p class="hero-headline">{{ __('Neka te svi vide !') }}</p>
-                <p class="hero-subtext"> {{ __('Poveži se sa') }} <span id='text' class="transition-text"></span></p>
-            </div>
-            <div class="d-flex gap-3 col-sm-12 slider-buttons">
-                <a href="{{ route('register') }}">
-                    <button class="btn btn-primary" type="button">{{ __('Napravi novi profil') }}</button>
-                </a>
-                <a href="{{ route('auth.login') }}">
-                    <button class="btn btn-outline-light" type="button">{{ __('Prijava na postojeći profil') }}</button>
-                </a>
+{{--<section class="hero container-fluid border-1">--}}
+{{--    <div class="container">--}}
+{{--        <div class="row d-grid g-4 align-items-end">--}}
+{{--            <div class="col-sm-12 headline">--}}
+{{--                <p class="hero-headline">{{ __('Neka te svi vide !') }}</p>--}}
+{{--                <p class="hero-subtext"> {{ __('Poveži se sa') }} <span id='text' class="transition-text"></span></p>--}}
+{{--            </div>--}}
+{{--            <div class="d-flex gap-3 col-sm-12 slider-buttons">--}}
+{{--                <a href="{{ route('register') }}">--}}
+{{--                    <button class="btn btn-primary" type="button">{{ __('Napravi novi profil') }}</button>--}}
+{{--                </a>--}}
+{{--                <a href="{{ route('auth.login') }}">--}}
+{{--                    <button class="btn btn-outline-light" type="button">{{ __('Prijava na postojeći profil') }}</button>--}}
+{{--                </a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</section>--}}
+
+
+
+<div class="main-slider">
+    <div class="main-slider-wrapper">
+        <div class="main-text">
+            <div class="center-div">
+                <div class="text-wrapper">
+                    <div class="let-everybody">
+                        <h1>NEKA TE</h1>
+                        <h1>SVI VIDE</h1>
+                    </div>
+                    <div class="connect-with">
+                        <h2>
+                            <p>Poveži se sa</p>
+                            <span id="text"> selektorima, klubovima, agentima, medijima... </span>
+                        </h2>
+                    </div>
+                </div>
+                <div class="buttons-wrapper">
+                    <a href="">
+                        <button> PRIJAVITE SE </button>
+                    </a>
+                    <p>
+                        Nemate svoj profil? <a href="">Registrujte se!</a>
+                    </p>
+
+                    <div class="arrow-down">
+                        <i class="fas fa-arrow-down"></i>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</section>
+</div>
 
 
 <script>
-    typing_effect(['selektorima...', 'klubovima...', 'agentima...', 'medijima...'],
-        ['#d5d35a']);
+    typing_effect(['selektorima', 'klubovima', 'agentima', 'medijima'], ['#2AA7B8']);
 
     function typing_effect(words, colors) {
+        if(window.innerWidth >= 800) return;
 
         var cursor = document.getElementById('cursor'); //cursor
         var text = document.getElementById('text'); //text
@@ -50,7 +85,7 @@
             if (letters_count === 0 && wait === false) {
                 wait = true;
 
-                text.innerHTML = '' // leave a blank
+                text.innerHTML = ' ... ' // leave a blank
                 window.setTimeout(function () {
                     var usedColor = colors.splice(0, 1)[0] //remove first element and get it as str
                     colors.push(usedColor);
